61d426ffc6136c0e3cad87184fb7f2f83d45b45c
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-10 14:25:22 -0600

    adding time blocks

--- script.js
+++ script.js
@@ -7,36 +7,56 @@ var hour2 = $("#2")
 var hour3 = $("#3")
 var hour4 = $("#4")
 var hour5 = $("#5")
-var time = moment()
-// console.log(time)
+console.log(hour9)
+var currentTime = moment().format("hh:mm a");
+console.log("the time is ", currentTime)
+
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-function updateHours(){
-var hour = time.hour()
-console.log(time.hour())
+function updateHourly(){
+    for (let i = 9; i < 18 ; i++) {
+        console.log(i)
 
-    // if time has passed set color 
-    if (moment().isAfter(hour)) {
-        $(".hour").addClass("past")
-        // console.log(this)
+     // if time has passed set color 
+        if (currentTime < i){
+            $(".hour" + i).addClass("past")
+        }
+        // if time is current 
+        else if (i == currentTime){
+            $(".hour").addClass("present")
+        }
+
+        else{
+            $(".hour").addClass("future")
+
+        }
     }
-
-    // if time is current 
-else if (moment () = hour){
-    $(".hour").addClass("current")
-
-}
-    // if time is in the future
-else {
-    $(".hour").addClass("future")
 }
 
-}
+// function updateHours(){
+
+//     // if time has passed set color 
+//     if (moment().isAfter(hour)) {
+//         $(".hour").addClass("past")
+//         console.log("you have ", (moment().isAfter(hour)) )
+//     }
+
+//     // if time is current 
+// else if (moment () = hour){
+//     $(".hour").addClass("current")
+
+// }
+//     // if time is in the future
+// else {
+//     $(".hour").addClass("future")
+// }
+
+// }
 
 function savedInput (){
     task = JSON.parse(localStorage.getItem("plan"));
-    
+
 
 }
 
@@ -45,6 +65,7 @@ function savedInput (){
 
 $(".saveBtn").click(function (){
 var task = $(this).siblings("textarea").val()
+
 console.log($(this).siblings("textarea").val())
 
 localStorage.setItem("plan", JSON.stringify(task))
