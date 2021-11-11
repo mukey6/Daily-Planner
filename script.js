b1dd77fa4e8fb60ea06a212b87e77fa2b7330c88
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-10 22:25:45 -0600

    time block format

--- script.js
+++ script.js
@@ -7,53 +7,35 @@ var hour2 = $("#2")
 var hour3 = $("#3")
 var hour4 = $("#4")
 var hour5 = $("#5")
-console.log(hour9)
 var currentTime = moment().hour();
-console.log("the time is ", currentTime)
 
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 function updateHourly(){
-    for (let i = 9; i < 18 ; i++) {
-        console.log(i)
-console.log(currentTime)
+    for (let i = 9; i <= 18 ; i++) {
 
      // if time has passed set color 
-        if (currentTime < i){
-            $(".hour" + i).addClass("past")
+        console.log("current time is " + currentTime)
+        console.log("What is this " + i)
+        console.log("current time is greater than ", currentTime>i)
+     if (currentTime > i){
+            $(".description").addClass("past")
+            console.log("past")
         }
         // if time is current 
-        else if (i == currentTime){
-            $(".hour").addClass("present")
-        }
+         if (currentTime == i){
+            $(".description").addClass("present")
+            console.log("present")
+        }  
+        // if (currentTime < i){
+        //     $(".description").addClass("future")
 
-        else{
-            $(".hour").addClass("future")
-
-        }
+        // }   
     }
 }
 
-// function updateHours(){
 
-//     // if time has passed set color 
-//     if (moment().isAfter(hour)) {
-//         $(".hour").addClass("past")
-//         console.log("you have ", (moment().isAfter(hour)) )
-//     }
-
-//     // if time is current 
-// else if (moment () = hour){
-//     $(".hour").addClass("current")
-
-// }
-//     // if time is in the future
-// else {
-//     $(".hour").addClass("future")
-// }
-
-// }
 
 function savedInput (){
     task = JSON.parse(localStorage.getItem("plan"));
@@ -71,3 +53,5 @@ console.log($(this).siblings("textarea").val())
 
 localStorage.setItem("plan", JSON.stringify(task))
 })
+
+updateHourly()
